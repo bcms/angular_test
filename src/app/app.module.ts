3820e688daa6fba54592868a673feeffc1b06593
Bruno Silva <bruno.silva@sage.com>
2019-12-05 12:32:34 -0300

    Finalizado dialog do login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginDialogComponent } from './login/dialog/login-dialog/login-dialog.component';
-import { AaaComponent } from './aaa/aaa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginDialogComponent,
-    AaaComponent
+    LoginDialogComponent
   ],
   imports: [
     BrowserModule,
